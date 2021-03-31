ec6a031de4dbc189681a5a05a02003f14908c344
GabrielHorbach <gabrielhorbachmachado@hotmail.com>
2021-03-31 02:42:00 -0300

    feat: start to configure PWA

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'bring-your-umbrella-app';
+
+const urlsToCache = [
+  '/',
+];
+
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache)),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }),
+  );
+});
